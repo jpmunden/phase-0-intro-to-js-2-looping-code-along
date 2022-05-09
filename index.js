ca423bee449ba9aba4cf2648c0b01cbcd8817597
Jacob <jacobmunden@gmail.com>
2022-05-09 09:43:26 -0400

    commit tree 208318b8049fc656a95cf4ff383999de6d488ea0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+const names = ["Charlie", "Samip", "Ali"]
+
+function writeCards(names, occasion) {
+  let birthday =[];
+  occasion = "surprise";
+  for (let i= 0; i < names.length; i++) {
+  birthday[i] = 'Thank you, '+ names[i] +', for the wonderful ' +occasion+' gift!';
+  debugger;
+  }
+  return birthday;
+}
+
+number = 10;
+function countDown(number) {
+  while (number >= 0) {
+    console.log(number--);
+  }
+}
